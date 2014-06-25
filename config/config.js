da96ee6e8fca317f3a312c8ef3fba304151f9606
neutrinocoin <admin@neutrinocoin.org>
2014-06-25 15:18:06 -0700

    update config.js with Neutrino ports and paths

--- config/config.js
+++ config/config.js
@@ -23,15 +23,15 @@ if (process.env.INSIGHT_NETWORK === 'livenet') {
   env = 'livenet';
   db = home;
   port = '3000';
-  b_port = '8332';
-  p2p_port = '8333';
+  b_port = '8776';
+  p2p_port = '8777';
 }
 else {
   env = 'testnet';
   db = home + '/testnet';
   port = '3001';
-  b_port = '18332';
-  p2p_port = '18333';
+  b_port = '18776';
+  p2p_port = '18777';
 }
 
 
@@ -54,9 +54,9 @@ var isWin = /^win/.test(process.platform);
 var isMac = /^darwin/.test(process.platform);
 var isLinux = /^linux/.test(process.platform);
 if (!dataDir) {
-  if (isWin) dataDir = '%APPDATA%\\Bitcoin\\';
-  if (isMac) dataDir = process.env.HOME + '/Library/Application Support/Bitcoin/';
-  if (isLinux) dataDir = process.env.HOME + '/.bitcoin/';
+  if (isWin) dataDir = '%APPDATA%\\Neutrinocoin\\';
+  if (isMac) dataDir = process.env.HOME + '/Library/Application Support/Neutrinocoin/';
+  if (isLinux) dataDir = process.env.HOME + '/.neutrinocoin/';
 }
 dataDir += network === 'testnet' ? 'testnet3' : '';
 
